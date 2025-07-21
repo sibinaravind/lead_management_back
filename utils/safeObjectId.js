90b7e1c0aad64fb1a567c2dc6f60359c88cb8092
Sibin james <sibinjames@Sibins-MacBook-Air.local>
2025-07-21 13:25:58 +0530

    added validation for all

--- utils/safeObjectId.js
+++ utils/safeObjectId.js
@@ -1,9 +1,11 @@
 const { ObjectId } = require('mongodb');
 
-export function safeObjectId(id) {
+function safeObjectId(id) {
   if (id instanceof ObjectId) return id;
 
   return typeof id === 'string' && /^[0-9a-fA-F]{24}$/.test(id)
     ? new ObjectId(id)
     : null;
 }
+
+module.exports = { safeObjectId };
