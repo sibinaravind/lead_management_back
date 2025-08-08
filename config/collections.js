8605c9056e11aa453400a8e610d088fc1cbcc003
Sibin james <sibinjames@Sibins-MacBook-Air.local>
2025-08-08 16:06:14 +0530

    nginx

---
+++ config/collections.js
@@ -0,0 +1,22 @@
+module.exports={
+//main db
+    ADMIN_COLLECTION:'admin',
+    OFFICERS:'officers',
+    POST:'post',
+    CONFIG:'config',
+    CLIENTS:'clients',
+    COUNTER: 'counter',
+    PROJECTS: 'projects',
+    VACANCIES: 'vacancies',
+    CUSTOMERS:'customers',
+    LEADS: 'leads',
+    CAMPAIGNS: 'campaigns',
+    CUSTOMER_ACTIVITY: 'customer_activity',
+    CALL_LOG_ACTIVITY:'call_log_activity',
+    DEAD_LEADS: 'dead_leads',
+    ROUNDROBIN: 'roundrobin',
+    ANNOUNCEMENTS: 'announcements',
+    
+   
+
+}
